171c13b87b067574d2c5bfbd47724dfbf164a871
rdebeer1 <Robert.DeBeer@gmail.com>
2018-02-07 15:35:30 -0600

    renders index.html but not stylesheets

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,11 +11,41 @@ exports.headers = {
 };
 
 exports.serveAssets = function(res, asset, callback) {
+  // if (req.url === '/' || req.ulr === '/index.html') {
+    // console.log('res ', res);
+    console.log();
+    fs.readFile(asset);
+    // , function(err, data) {
+    //   if (err) {
+    //     console.log('done fucked up');
+    //     // callback ? callback() : this.send404(res);
+    //   } else {
+    //     this.respond(res, data);
+    //   }
+    // }); 
+    
+      //path.basename())
+  
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  
+  //look in public and serve those files
+    //file does not exist in public - look in archive and serve 
+      //file does not exist in archive - alternate response or 404
+      //serve archived files
+    //serve public files
 };
 
+exports.respond = (res, data, status) => {
+  status = status || 200;
+  res.writeHead(status, this.headers);
+  res.end(data);
+};
 
+exports.send404 = (res) => {
+  exports.respond(res, 'Not Found', 404);
+};
+ 
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,45 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var helper = require('../web/http-helpers');
+var fs = require('fs');
+
+var actions = {
+  'Get': function (req, res) {
+    helper.respond(res, data, status);
+  },
+  
+};
+
+
+
+
+
+
+
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('handleRequest');
+  console.log('req.url ', req.url);
+  if (req.url === '/') {
+    fs.readFile(archive.paths.siteAssets + '/index.html', function(error, content) {
+      if (error) {
+        console.log('error', error);
+        res.writeHead(500);
+        res.end();
+      } else {
+        console.log('readfile');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(content);
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+  // console.log(actions[req.method]);
+  // helper.serveAssets(res, archive.paths.siteAssets);
+  // var action = actions[req.method];
+  // console.log('action ', action);
+  // action ? helper.serveAssets(res, asset, callBack) : helper.send404(res);
 };
